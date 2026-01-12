966f4b5f34aa313846fe64df3a352c8564da2695
Milan BHimani <milanhbhimani@gmail.com>
2026-01-12 19:10:23 +0530

    Version 2

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,20 @@
-$Repo = "ghcr.io/milan-bhimani/cd"
-$Tag = "main"
-$Image = "$Repo`:$Tag"
+# Configuration
+$Image = "ghcr.io/milan-bhimani/cd:main"
 $ContainerName = "production-app"
-Write-Host "🚀 Starting Deployment Simulation..." -ForegroundColor Cyan
-Write-Host "⬇️ Pulling latest image..."
+
+Write-Host "--- Starting Deployment ---" -ForegroundColor Cyan
+
+# 1. Pull the image
+Write-Host "Step 1: Pulling image..."
 docker pull $Image
-Write-Host "🛑 Stopping old container..."
-docker stop $ContainerName 2>$null
-docker rm $ContainerName 2>$null
-Write-Host " Starting new version..."
-docker run -d -p 8080:8000 --name $ContainerName$Image
-Write-Host "✅ Deployment Complete! App is live at http://localhost:8080" -ForegroundColor Green
+
+# 2. Cleanup old container
+Write-Host "Step 2: Removing old container..."
+docker rm -f $ContainerName 2>$null
+
+# 3. Start new container
+Write-Host "Step 3: Starting new container..."
+docker run -d -p 8080:8000 --name $ContainerName $Image
+
+Write-Host "--- Deployment Finished! ---" -ForegroundColor Green
+Write-Host "Visit: http://localhost:8080"
--- practice.py
+++ practice.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Version 2."
+    return "Hello Version 2.HEHEHe"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
