5449dcf873a66a5c50e92b0eaeb6e0b6cf603518
Milan BHimani <milanhbhimani@gmail.com>
2026-01-12 19:02:36 +0530

    Version 2

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+$Repo = "ghcr.io/milan-bhimani/cd"
+$Tag = "main"
+$Image = "$Repo`:%Tag"
+$ContainerName = "production-app"
+Write-Host "🚀 Starting Deployment Simulation..." -ForegroundColor Cyan
+Write-Host "⬇️ Pulling latest image..."
+docker pull $Image
+Write-Host "🛑 Stopping old container..."
+docker stop $ContainerName 2>$null
+docker rm $ContainerName 2>$null
+Write-Host " Starting new version..."
+docker run -d -p 8080:8000 --name $ContainerName$Image
+Write-Host "✅ Deployment Complete! App is live at http://localhost:8080" -ForegroundColor Green
